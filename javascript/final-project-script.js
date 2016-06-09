a464c231386a56f2486168fd7f708d1db607f221
Patsyklotz <patsy.neher@gmail.com>
2016-06-09 08:11:22 -0700

    new js code

---
+++ javascript/final-project-script.js
@@ -0,0 +1,50 @@
+function toggle(id) 
+{
+    var e = document.getElementById(id);
+    if (e.style.display == 'none' || e.style.display=='') 
+    {
+      e.style.display = 'block';
+    }
+    else
+    {
+      e.style.display = 'none';
+    }
+}
+
+function show_hide(id) 
+{
+    var e = document.getElementById(id);
+    if (e.style.display == 'none' || e.style.display=='none') 
+    {
+      e.style.display = 'block';
+    }
+    else
+    {
+      e.style.display = 'none';
+    }
+}
+function appear(id) 
+{
+    var e = document.getElementById(id);
+    if (e.style.display == 'none' || e.style.display=='none') 
+    {
+      e.style.display = 'block';
+    }
+    else
+    {
+      e.style.display = 'none';
+    }
+}
+
+function visible(id) 
+{
+    var e = document.getElementById(id);
+    if (e.style.display == 'none' || e.style.display=='none') 
+    {
+      e.style.display = 'block';
+    }
+    else
+    {
+      e.style.display = 'none';
+    }
+}
